c61697ee89ea95b2e989d17719d2682b8ff8a28d
zhaochao <zhaochao@rd.netease.com>
2018-02-21 21:22:34 +0800

    add

--- http/writefile.js
+++ http/writefile.js
@@ -3,16 +3,30 @@ var fs = require('fs');
 var url = require('url');
 
 
+
+var router = (req, res) =>{
+  var pathname = url.parse(req.url).pathname.replace(/\// , '');
+  if(pathname == 'writefile'){
+    let data = 'test';
+    writefile('./file/writefile.txt' , data , () => {
+      console.log('writefile done');
+    }) 
+  }   
+}
+
+
 http.createServer(function(req , res){
   res.writeHead(200, {'Content-Type':'text/plain;charset=utf-8'});
-  res.write('hello world');
-  res.end('');
+  res.write('创建文件');
+  router(req, res);
+  res.end('');  
 }).listen(2018)
 
 
 
+
 function writefile(file, data, callBack){
-   fs.writefile(file, data , (err)=>{
+   fs.writeFile(file, data , (err)=>{
      if(err) throw err;
      callBack && callBack();
    })
@@ -20,6 +34,6 @@ function writefile(file, data, callBack){
 
 
 function writefilesync(file, data, callBack){
-   fs.writefilesync(file , data);
+   fs.writeFileSync(file , data);
    callBack && callBack();
 }
